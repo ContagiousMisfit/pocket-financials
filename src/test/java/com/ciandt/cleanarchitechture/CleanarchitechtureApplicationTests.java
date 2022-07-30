c1eef980757021d4c77ae457b95e56b26b26a478
Italo Gomes <italogomes@ciandt.com>
2022-07-30 13:57:47 -0300

    First commit.

---
+++ src/test/java/com/ciandt/cleanarchitechture/CleanarchitechtureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciandt.cleanarchitechture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchitechtureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
