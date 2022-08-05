dae9d569e230c25a28d5c8879ffe64b58edb8c0a
ContagiousMisfit <isacaovila@gmail.com>
2022-08-05 16:50:58 -0300

    Adding frontend support for AXIOS

--- src/main/java/com/ciandt/cleanarchitechture/infrastructure/controller/posting/ListFinancialPostingController.java
+++ src/main/java/com/ciandt/cleanarchitechture/infrastructure/controller/posting/ListFinancialPostingController.java
@@ -9,13 +9,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/post/list")
 public class ListFinancialPostingController {
